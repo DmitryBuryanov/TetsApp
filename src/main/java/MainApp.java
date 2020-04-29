ed321b9fd60aca5abcaf3c0743c01f7a5fb2ef57
DmitryBuryanov <buryanov.dv@edu.spbstu.ru>
2020-04-29 21:04:57 +0300

    игра реализована

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -240,8 +240,7 @@ public class MainApp extends Application {
         }
         return "";
     }
-
-
+    
     public boolean cellExist(int x, int y) {
         return x >= 0 && x <= 7 && y >= 0 && y <= 7;
     }
